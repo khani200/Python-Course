b3b2e430b21fdda3b9dc5e8897343d0eb17ce88b
Muhammad Ali Haider <43770212+khani200@users.noreply.github.com>
2021-09-12 16:08:25 +0500

    Add files via upload

---
+++ Class1/class_1oop.py
@@ -0,0 +1,18 @@
+class employee:
+
+    def __init__(self, first, last, pay):
+        self.first=first
+        self.last=last
+        self.pay=pay
+        self.email=first+"."+last+"@smme.com"
+    def fullname(self):
+        return "{} {}".format(self.first,self.last)
+
+
+emp_1=employee("Ali","Haider",9500)
+emp_2=employee("Daud","Iqbal",10000)
+
+print(emp_1.fullname())
+print(emp_1.email)
+print(emp_2.fullname())
+print(emp_2.email)
